578362b3414562f78913f1588cae0789177e4807
kiranDhoni8147 <130906111+kiranDhoni8147@users.noreply.github.com>
2024-06-14 17:27:35 +0530

    Base URL modified

--- client/src/service/axiosConfig.js
+++ client/src/service/axiosConfig.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //use deployment url for production
-  baseURL: 'http://localhost:5000/api',
+  baseURL:'https://global-expert-backend.onrender.com',
+  // baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
